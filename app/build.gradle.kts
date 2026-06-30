29934ffda10aedc0204245fb8884bc64c6553fea
Ali mahjoob <behnamuiux@gmail.com>
2026-06-30 18:33:27 +0330

    edit ui and improve it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.behnamuix.spygame"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.1.0"
+        versionCode = 5
+        versionName = "2.2.0"
 
         buildConfigField(
             "String",
