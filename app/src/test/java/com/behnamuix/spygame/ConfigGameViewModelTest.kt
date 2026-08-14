f618dd1b87f54e1fe06ec1fbbf5901f46e823c1e
Ali mahjoob <behnamuiux@gmail.com>
2026-08-14 23:09:01 +0330

    add test

--- app/src/test/java/com/behnamuix/spygame/ConfigGameViewModelTest.kt
+++ app/src/test/java/com/behnamuix/spygame/ConfigGameViewModelTest.kt
@@ -1,4 +1,56 @@
 package com.behnamuix.spygame
 
+import com.behnamuix.spygame.core.media.viewmodel.MediaPlayerViewModel
+import com.behnamuix.spygame.data.local.db.repository.keyword.KeywordRepository
+import com.behnamuix.spygame.feature.configgame.domain.usecase.ConfigGameUseCase
+import com.behnamuix.spygame.feature.configgame.presentation.viewmodel.ConfigGameViewModel
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.whenever
+import kotlin.test.Test
+
 class ConfigGameViewModelTest {
+    private val dispatcher = StandardTestDispatcher()
+    private val keywordRepo = mock<KeywordRepository>()
+    private val mediaVm = mock<MediaPlayerViewModel>()
+    private val useCase = mock<ConfigGameUseCase>()
+
+    private lateinit var viewModel: ConfigGameViewModel
+
+    @Before
+    fun setup(){
+        //az in dispacher estefade kon
+        Dispatchers.setMain(dispatcher)
+        viewModel = ConfigGameViewModel(
+            keywordRepo,
+            mediaVm,
+            useCase
+        )
+    }
+    //bad az test
+    @After
+    fun tearDown() {
+        //dispacher ro bargardon be ghabl
+        Dispatchers.resetMain()
+    }
+    @Test
+    fun `inc agent count should update agent count`() = runTest {
+
+        whenever(useCase.incAgentCountPlayer())
+            .thenReturn(2)
+
+        viewModel.incAgentCountPlayer()
+
+        advanceUntilIdle()
+
+        assertEquals(2, viewModel.agentCount.value)
+    }
 }
